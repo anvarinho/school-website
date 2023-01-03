c3d946f7ade5c7dfbb11a7d9cbc1d016f9dac6b2
anvarinho <anvarinho@gmail.com>
2023-01-03 20:56:11 +0300

    first commit

---
+++ static/js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+    const element = document.querySelector('nav');
+    if (scrollPos == 0) {
+        element.classList.remove('window-scroll');
+      } else {
+        element.classList.add('window-scroll');
+      }
+    // document.querySelector('nav').classList.toggle('window-scroll', window.screenY > 15);
+});
+
+const faqs = document.querySelectorAll('.faq')
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+        const icon = faq.querySelector('.faq__icon i')
+        if(icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus'
+        }else{
+            icon.className = 'uil uil-plus'
+        }
+    })
+})
+
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex'
+    closeBtn.style.display = 'inline-block'
+    menuBtn.style.display = 'none'
+})
+
+const closeNav = () => {
+    menu.style.display = 'none'
+    closeBtn.style.display = 'none'
+    menuBtn.style.display = 'inline-block'
+}
+
+closeBtn.addEventListener('click', closeNav)
